2385dd7c73a62abf42b5b6c929d89a1218509b22
zuytaipingc <627097073@qq.com>
2025-04-19 20:15:21 +0800

    vue3_1

---
+++ vue3_1/src/App.vue
@@ -0,0 +1,15 @@
+<script>
+import Hello from './components/Hello.vue';
+export default{
+  name:'App',
+  components:{helloHello}
+}
+</script>
+<template>
+    <div class="app">
+      <h1>你好啊</h1>
+
+    </div>
+    <hello></hello>
+
+</template>
---
+++ vue3_1/src/components/Hello.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="app">
+        <h1>张三</h1>
+
+    </div>
+</template>
+
+<script>
+    export default{
+        name:'App'
+    }
+</script>
+
+<style>
+    .app{
+        background-color: blue;
+    }
+</style>
